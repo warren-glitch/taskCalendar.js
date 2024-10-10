2891f797b5632fdd0e1041867f827ba6388770a3
warren-glitch <warrenpiano24@gmail.com>
2024-10-11 07:40:05 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,20 @@
+let day = prompt("Enter a day in a week:");
+switch (day) {
+    case "Monday":
+        alert("You have a online class today.");
+        break;
+    case "Tuesday":
+        alert("Do your House shores.");
+        break;
+    case "Wednesday":
+        alert("Your dog has a vaccine appointment.");
+        break;
+    case "Thursday":
+        alert("You have a 6 activites due today.");
+        break;
+    case "Friday":
+        alert("Do your project.");
+        break;
+    default:
+        alert("Saturday or Sunday is rest day.");
+}
